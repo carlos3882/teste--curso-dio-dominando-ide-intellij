df421282d7488828fe8c32ea40bccbc3e5024ed6
Joaquim Carlos Correa Siqueira de Carvalho <profcarlosmcinfo@gmail.com>
2021-11-04 22:19:34 -0300

    Criando calculadora

---
+++ src/br/com/dio/calculadora/calculadora.java
@@ -0,0 +1,41 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a;
+        int b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("O resultado da soma é: " + somar);
+        System.out.println("O resultado da subitração é: " + subtrair);
+        System.out.println("O resultado da multiplicação é: " + multiplicar);
+        System.out.println("O resultado da divisão é: " + dividir);
+
+    }
+
+    public static int somar(int a, int b) {
+        return a + b;
+    }
+    public static int subtrair(int a, int b) {
+        return a - b;
+    }
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+}
